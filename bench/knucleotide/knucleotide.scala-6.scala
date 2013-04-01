501a9ace14478c5f230df96aaf6de605f70b92d0
igouy-guest <igouy-guest>
2013-04-01 17:16:02 +0000

    *** empty log message ***
    SKIPPED:
    	website/websites/u64/code/knucleotide.6.scala.code
    	website/websites/u64/code/knucleotide.6.scala.log
    	website/websites/u64/code/mandelbrot.2.gpp.code
    	website/websites/u64/code/mandelbrot.2.gpp.log
    	website/websites/u64/code/nbody.2.ghc.code
    	website/websites/u64/code/nbody.2.ghc.log
    	website/websites/u64/data/data.csv
    	website/websites/u64/data/ndata.csv
    	website/websites/u64/data/u64_bulkdata.csv.bz2
    	website/websites/u64q/code/knucleotide.6.scala.code
    	website/websites/u64q/code/knucleotide.6.scala.log
    	website/websites/u64q/code/mandelbrot.2.gpp.code
    	website/websites/u64q/code/mandelbrot.2.gpp.log
    	website/websites/u64q/code/nbody.2.ghc.code
    	website/websites/u64q/code/nbody.2.ghc.log
    	website/websites/u64q/data/data.csv
    	website/websites/u64q/data/ndata.csv
    	website/websites/u64q/data/u64q_bulkdata.csv.bz2

--- bench/knucleotide/knucleotide.scala-6.scala
+++ bench/knucleotide/knucleotide.scala-6.scala
@@ -3,16 +3,24 @@
    Based partially on the version by The Anh Tran
    Based on bit encoding idea of C++ contribution of Andrew Moon
    Contributed by Mark Hammons
+   Updated for 2.10.0 by Mark Hammons
 */
 
+import akka.actor.ActorSystem
 import annotation.tailrec
 import java.util.HashMap
 import java.io.{InputStreamReader, BufferedReader}
-import scala.actors.Futures._
-import scala.actors.Future
+
+import concurrent.{Await, ExecutionContext, Future}
+import concurrent.duration.Duration
 
 
 object knucleotide extends App {
+
+  val as = ActorSystem.create("futures")
+
+  implicit val ec: ExecutionContext = as.dispatcher
+
   val reader = new BufferedReader(new InputStreamReader(System.in,  "US-ASCII"), 8*1024*1024)
 
   var buffer = "lola"
@@ -22,7 +30,7 @@ object knucleotide extends App {
 
   type mapSeqType = List[(Future[NucleoHashMap],(String,Int))]
 
-  val digestibleSeq:mapSeqType = List(1,2,3,4,6,12,18).map(frame => future{new NucleoHashMap(frame)} -> ("",frame))
+  val digestibleSeq:mapSeqType = List(1,2,3,4,6,12,18).map(frame => Future(new NucleoHashMap(frame)) -> ("",frame))
 
   @tailrec
   def digester(maps: mapSeqType, length: Int = 0): (mapSeqType,Int) = {
@@ -33,11 +41,11 @@ object knucleotide extends App {
 
     val input = in.stripLineEnd.toLowerCase
 
-    val f = maps.map(x => x._1() -> x._2)
+    val f = maps.map(x => Await.result(x._1, Duration.Inf) -> x._2)
 
     def getRemainder(str: String, frame: Int) = str.substring(str.length - (frame-1))
     val res = for((map,(remainder,frame)) <- f) yield
-      future{
+      Future{
         val in = remainder + input
         map.process(in)
         map
@@ -50,7 +58,7 @@ object knucleotide extends App {
 
   val (retList,length) = digester(digestibleSeq)
 
-  val maps = retList.map(_._1())
+  val maps = retList.map(r => Await.result(r._1, Duration.Inf))
 
   def lookup(str: String): Int = {
     val l = str.length
@@ -65,18 +73,20 @@ object knucleotide extends App {
   val doubles = for(i <- componentList; j <- componentList)
     yield (i+j).toUpperCase -> (lookup(i+j)/length.toDouble * 100)
 
-  singles.sortWith(_._2 > _._2).foreach(a => {printf("%s %.3f\n",a._1,a._2); /*println(lookup(a._1.toLowerCase))*/})
+  singles.sortWith(_._2 > _._2).foreach(a => {printf("%s %.3f\n",a._1,a._2);})
   println("")
-  doubles.sortWith(_._2 > _._2).foreach(a => {printf("%s %.3f\n",a._1,a._2); /*println(lookup(a._1.toLowerCase))*/})
+  doubles.sortWith(_._2 > _._2).foreach(a => {printf("%s %.3f\n",a._1,a._2);})
 
   println("")
 
   List("GGT","GGTA","GGTATT", "GGTATTTTAATT", "GGTATTTTAATTTATAGT").foreach(
     s => println(lookup(s.toLowerCase) + "\t" + s)
   )
+
+  as.shutdown()
 }
 
-class NucleoHashMap(val frame: Int) {
+class NucleoHashMap(val frame: Int)(implicit ec: ExecutionContext) {
   class BabyHash[K] extends HashMap[K,Count] {
   @inline final def apply(key: K): Count = {
       val r = this.get(key)
@@ -105,7 +115,7 @@ class NucleoHashMap(val frame: Int) {
       }
     }
 
-    maps.map(x => future(addStrings(x._1,str.substring(x._2)))).map(_())
+    maps.map(x => Future{addStrings(x._1,str.substring(x._2))}).map(Await.result(_, Duration.Inf))
   }
 
   def apply(str: String):Int = {
@@ -120,3 +130,4 @@ class Count {
 
   @inline final def +(v: Int) = count + v
 }
+ 
