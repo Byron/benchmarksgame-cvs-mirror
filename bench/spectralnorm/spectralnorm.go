e1a648a689ab9ab9096d906363b838caee46a8e3
igouy-guest <igouy-guest>
2013-05-16 18:28:33 +0000

    *** empty log message ***
    SKIPPED:
    	website/websites/u64/code/nbody.4.gcc.code
    	website/websites/u64/code/nbody.4.gcc.log
    	website/websites/u64/code/spectralnorm.1.go.code
    	website/websites/u64/code/spectralnorm.1.go.log
    	website/websites/u64/data/data.csv
    	website/websites/u64/data/ndata.csv
    	website/websites/u64/data/u64_bulkdata.csv.bz2
    	website/websites/u64q/code/nbody.4.gcc.code
    	website/websites/u64q/code/nbody.4.gcc.log
    	website/websites/u64q/code/spectralnorm.1.go.code
    	website/websites/u64q/code/spectralnorm.1.go.log
    	website/websites/u64q/data/data.csv
    	website/websites/u64q/data/ndata.csv
    	website/websites/u64q/data/u64q_bulkdata.csv.bz2

---
+++ bench/spectralnorm/spectralnorm.go
@@ -0,0 +1,65 @@
+/* The Computer Language Benchmarks Game
+ * http://shootout.alioth.debian.org/
+ *
+ * contributed by chaishushan
+ * Based on spectral-norm.go by The Go Authors.
+ */
+
+package main
+
+import (
+   "flag"
+   "fmt"
+   "math"
+)
+
+var n = flag.Int("n", 2000, "count")
+
+func evalA(i, j int) int { return ((i+j)*(i+j+1)/2 + i + 1) }
+
+type Vec []float64
+
+func (v Vec) Times(u Vec) {
+   for i := 0; i < len(v); i++ {
+      v[i] = 0
+      for j := 0; j < len(u); j++ {
+         v[i] += u[j] / float64(evalA(i, j))
+      }
+   }
+}
+
+func (v Vec) TimesTransp(u Vec) {
+   for i := 0; i < len(v); i++ {
+      v[i] = 0
+      for j := 0; j < len(u); j++ {
+         v[i] += u[j] / float64(evalA(j, i))
+      }
+   }
+}
+
+func (v Vec) ATimesTransp(u Vec) {
+   x := make(Vec, len(u))
+   x.Times(u)
+   v.TimesTransp(x)
+}
+
+func main() {
+   flag.Parse()
+
+   N := *n
+   u := make(Vec, N)
+   for i := 0; i < N; i++ {
+      u[i] = 1
+   }
+   v := make(Vec, N)
+   for i := 0; i < 10; i++ {
+      v.ATimesTransp(u)
+      u.ATimesTransp(v)
+   }
+   var vBv, vv float64
+   for i := 0; i < N; i++ {
+      vBv += u[i] * v[i]
+      vv += v[i] * v[i]
+   }
+   fmt.Printf("%0.9f\n", math.Sqrt(vBv/vv))
+}
