b13b253a1074aaa2d3e6ed4c811537fa704f7dfa
igouy-guest <igouy-guest>
2013-09-18 04:57:43 +0000

    *** empty log message ***
    SKIPPED:
    	website/websites/u64/code/pidigits.5.php.code
    	website/websites/u64/code/pidigits.5.php.log
    	website/websites/u64/code/revcomp.2.php.code
    	website/websites/u64/code/revcomp.2.php.log
    	website/websites/u64/data/data.csv
    	website/websites/u64/data/ndata.csv
    	website/websites/u64/data/u64_bulkdata.csv.bz2
    	website/websites/u64q/code/pidigits.5.php.code
    	website/websites/u64q/code/pidigits.5.php.log
    	website/websites/u64q/code/revcomp.2.php.code
    	website/websites/u64q/code/revcomp.2.php.log
    	website/websites/u64q/data/data.csv
    	website/websites/u64q/data/ndata.csv
    	website/websites/u64q/data/u64q_bulkdata.csv.bz2

---
+++ bench/revcomp/revcomp.php-2.php
@@ -0,0 +1,36 @@
+<?php
+#
+# The Computer Language Benchmarks Game
+# http://benchmarksgame.alioth.debian.org/
+#
+# reverse complement in PHP
+# contributed by Danny Sauer
+# modified by Craig Russell
+#
+
+
+ob_implicit_flush(1);
+ob_start(NULL, 4096);
+
+$seq = '';
+
+# read in the file, a line at a time
+do {
+    $str = trim(fgets(STDIN));
+    if( $str[0] == '>' ){
+        # if we're on a comment line, print the previous seq and move on
+		if($seq != ''){
+		   echo wordwrap( strrev( strtr(strtoupper($seq),
+			  'CGATMKRYVBHD', 'GCTAKMYRBVDH') ), 60, "\n", true ), "\n";
+		   $seq = '';
+		}
+        echo $str, "\n";
+    }else{
+        # otherwise, just append to the sequence
+        $seq .= $str;
+    }
+} while( !feof(STDIN) );
+if($seq != ''){
+	echo wordwrap( strrev( strtr(strtoupper($seq),
+	   'CGATMKRYVBHD', 'GCTAKMYRBVDH') ), 60, "\n", true ), "\n";
+}
