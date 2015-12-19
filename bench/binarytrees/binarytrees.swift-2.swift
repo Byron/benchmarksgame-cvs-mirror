91220e240e69fc3edd3fcb93062e09cf0f7058c0
igouy-guest <igouy-guest>
2015-12-19 21:50:28 +0000

    Swift SNAPSHOT-2015-12-18-a
    SKIPPED:
    	website/websites/u64q/swift.html
    	website/websites/u64q/code/binarytrees.2.swift.code
    	website/websites/u64q/code/binarytrees.2.swift.log

---
+++ bench/binarytrees/binarytrees.swift-2.swift
@@ -0,0 +1,63 @@
+/* The Computer Language Benchmarks Game
+   http://benchmarksgame.alioth.debian.org/
+   contributed by Isaac Gouy 
+*/
+
+import CoreFoundation
+
+
+class TreeNode {
+   var left, right : TreeNode?
+   var item : Int
+
+   init(_ left: TreeNode?, _ right: TreeNode?, _ item: Int) {
+      self.left = left
+      self.right = right
+      self.item = item
+   }
+
+   func check() -> Int {
+      guard let left = left, let right = right else {
+         return item 
+      }
+      return item + left.check() - right.check()
+   }
+}
+
+func bottomUpTree(item: Int, _ depth: Int) -> TreeNode {
+   if depth > 0 {
+      return 
+         TreeNode(
+            bottomUpTree(2*item-1, depth-1),
+            bottomUpTree(2*item, depth-1),
+            item
+         )
+   }
+   else {
+      return 
+         TreeNode(nil,nil,item)
+   }
+}
+
+
+let n: Int = Int(Process.arguments[1])!
+let minDepth = 4
+let maxDepth = n
+let stretchDepth = n + 1
+
+let check = bottomUpTree(0,stretchDepth).check()
+print("stretch tree of depth \(stretchDepth)\t check: \(check)")
+
+let longLivedTree = bottomUpTree(0,maxDepth)
+
+for var depth=minDepth; depth<=maxDepth; depth+=2 {
+   let iterations = 1 << (maxDepth - depth + minDepth)
+   var check = 0
+   for i in 0..<iterations {
+      check += bottomUpTree(i,maxDepth).check()
+      check += bottomUpTree(-i,maxDepth).check()
+   }
+   print("\(iterations*2)\t trees of depth \(depth)\t check: \(check)")
+}
+print("long lived tree of depth \(maxDepth)\t check: \(longLivedTree.check())")
+
