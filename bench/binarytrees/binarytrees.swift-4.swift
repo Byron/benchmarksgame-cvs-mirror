05a66911f9870919aae8f2a18cfba29de36d9276
igouy-guest <igouy-guest>
2015-12-22 01:25:08 +0000

    *** empty log message ***
    SKIPPED:
    	website/websites/u64q/code/mandelbrot.1.swift.code
    	website/websites/u64q/code/mandelbrot.1.swift.log
    	website/websites/u64q/data/data.csv
    	website/websites/u64q/data/ndata.csv
    	website/websites/u64q/data/u64q_bulkdata.csv.bz2

---
+++ bench/binarytrees/binarytrees.swift-4.swift
@@ -0,0 +1,86 @@
+/* The Computer Language Benchmarks Game
+   http://benchmarksgame.alioth.debian.org/
+
+   contributed by Alex Bustin
+*/
+
+
+import CoreFoundation
+
+struct TreeNode {
+   let item:Int
+   let left,right:UnsafeMutablePointer<TreeNode>
+   
+   func check() -> Int {
+      if left != nil {
+         return item + left.memory.check() - right.memory.check()
+      } else {
+         return item
+      }
+   }
+}
+
+
+func bottomUpTree(item:Int, _ depth: Int, _ node:UnsafeMutablePointer<TreeNode>, _ pool:COpaquePointer) {
+   
+   if depth > 0 {
+      let i = item*2
+      let d = depth-1
+      let l = UnsafeMutablePointer<TreeNode>.init(apr_palloc(pool,sizeof(TreeNode)))
+      let r = UnsafeMutablePointer<TreeNode>.init(apr_palloc(pool,sizeof(TreeNode)))
+      node.memory = TreeNode(item:item, left:l, right:r)
+      bottomUpTree(i-1, d, l, pool)
+      bottomUpTree(i, d, r, pool)
+   } else {
+      node.memory = TreeNode(item:item, left:nil, right:nil)
+   }
+   
+}
+
+let n: Int = Int(Process.arguments[1])!
+let minDepth = 4
+let maxDepth = n
+let stretchDepth = n + 1
+
+apr_initialize()
+var memoryPool = COpaquePointer()
+apr_pool_create_unmanaged_ex(&memoryPool,nil,nil)
+     
+var pool1 = COpaquePointer()
+apr_pool_create_unmanaged_ex(&pool1,nil,nil)
+
+var pool2 = COpaquePointer()
+apr_pool_create_unmanaged_ex(&pool2,nil,nil)
+
+var root = UnsafeMutablePointer<TreeNode>.init(apr_palloc(pool1,sizeof(TreeNode)))
+bottomUpTree(0,stretchDepth, root, pool1)
+let check = root.memory.check()
+print("stretch tree of depth \(stretchDepth)\t check: \(check)")
+ 
+root = UnsafeMutablePointer<TreeNode>.init(apr_palloc(pool2,sizeof(TreeNode)))
+bottomUpTree(0,maxDepth,root,pool2)
+var depth = minDepth
+
+while depth <= maxDepth {
+   let iterations = 1 << (maxDepth - depth + minDepth)
+   var check = 0
+   for i in 0..<iterations {
+      apr_pool_clear(pool1)
+      var l = UnsafeMutablePointer<TreeNode>.init(apr_palloc(pool1,sizeof(TreeNode)))
+      bottomUpTree(i,depth,l,pool1)
+      check += l.memory.check();
+      
+      apr_pool_clear(pool1)
+      l = UnsafeMutablePointer<TreeNode>.init(apr_palloc(pool1,sizeof(TreeNode)))
+      bottomUpTree(-i,depth,l,pool1)
+      check += l.memory.check();
+   
+   }
+   print("\(iterations*2)\t trees of depth \(depth)\t check: \(check)")
+   depth += 2
+}
+
+print("long lived tree of depth \(maxDepth)\t check: \(root.memory.check())")
+apr_pool_destroy(pool2)
+apr_pool_destroy(pool1)
+apr_terminate()
--- bench/mandelbrot/mandelbrot.swift
+++ bench/mandelbrot/mandelbrot.swift
@@ -2,7 +2,7 @@
    http://benchmarksgame.alioth.debian.org/
 
    direct transliteration of Greg Buchholz's C program
-   contributed by Isaac Gouy
+   contributed by Isaac Gouy, fix by David Turnbull
 */
 
 import Glibc
@@ -14,13 +14,14 @@ var bit_num = 0, i = 0, byte_acc: Int32 = 0
 let iter = 50, limit = 2.0
 var Zr, Zi, Cr, Ci, Tr, Ti: Double
 
-print("P4\n\(w) \(h)\n")
+print("P4\n\(w) \(h)")
 
 for y in 0..<h {
    for x in 0..<w {
 
       Zr = 0.0; Zi = 0.0; Tr = 0.0; Ti = 0.0
-      Cr = 2.0*Double(x)/Double(w) - 1.5; Ci = 2.0*Double(y)/Double(h) - 1.0
+      Cr = 2.0*Double(x)/Double(w) - 1.5; 
+      Ci = 2.0*Double(y)/Double(h) - 1.0
 
       i = 0
       while i < iter && (Tr+Ti <= limit*limit) {
