6ee9c511e5c8b97e136ec040b2210170f3657e95
igouy-guest <igouy-guest>
2016-03-02 03:30:28 +0000

    *** empty log message ***
    SKIPPED:
    	website/websites/u64q/code/revcomp.6.go.code
    	website/websites/u64q/code/revcomp.6.go.log
    	website/websites/u64q/data/data.csv
    	website/websites/u64q/data/ndata.csv
    	website/websites/u64q/data/u64q_bulkdata.csv.bz2

--- bench/revcomp/revcomp.go-6.go
+++ bench/revcomp/revcomp.go-6.go
@@ -12,6 +12,7 @@ import (
    "runtime"
 )
 
+const bufLines = 1024 * 16
 const lineSize = 60
 
 var complement = [256]uint8{
@@ -33,75 +34,138 @@ var complement = [256]uint8{
    'N': 'N', 'n': 'N',
 }
 
-func createoutput(buf []byte, obuf []byte, c chan int) {
-   lines := len(buf) / 60
-   bufpos := len(buf) - 1
-   for obufpos := 0; obufpos < lines*61; obufpos++ {
-      for end := obufpos + 60; obufpos < end; obufpos++ {
-         obuf[obufpos] = complement[buf[bufpos]]
-         bufpos--
+func createOutThread(inc chan [][]byte, statec chan int) {
+   work := <-inc
+   for {
+      inbuf := work[0]
+      outbuf := work[1]
+      lines := len(inbuf) / lineSize
+      linelength := lineSize
+      if lines == 0 {
+         linelength = len(inbuf)
       }
-      obuf[obufpos] = '\n'
+      for obufpos, bufpos := 0, len(inbuf)-1; bufpos >= 0; {
+         for end := obufpos + linelength; obufpos < end; obufpos++ {
+            outbuf[obufpos] = complement[inbuf[bufpos]]
+            bufpos--
+         }
+         outbuf[obufpos] = '\n'
+         obufpos++
+      }
+      statec <- 1
+      work = <-inc
    }
-   c <- 1
+}
+
+func doOutThread(inc chan []byte, statec chan int) {
+   work := <-inc
+   for {
+      os.Stdout.Write(work)
+      statec <- 1
+      work = <-inc
+   }
 }
 
 func main() {
    ncpu := runtime.NumCPU()
    runtime.GOMAXPROCS(ncpu)
+
    in := bufio.NewReader(os.Stdin)
-   out := bufio.NewWriter(os.Stdout)
-   buf := make([]byte, 0, 64*1024*1024)
+   buf := make([]byte, 0, lineSize*2*1024*1024)
+
+   prepstatec := make(chan int, ncpu)
+   var cpuic []chan [][]byte = make([]chan [][]byte, ncpu)
+   for i := 0; i < ncpu; i++ {
+      cpuic[i] = make(chan [][]byte, 1)
+      go createOutThread(cpuic[i], prepstatec)
+   }
+
+   outc := make(chan []byte, 1)
+   outstatec := make(chan int, 1)
+   outstatec <- 1
+   go doOutThread(outc, outstatec)
+
+   var obuf [2][]byte
+   obuf[0] = make([]byte, (lineSize+1)*bufLines*ncpu*2)
+   obuf[1] = obuf[0][(lineSize+1)*bufLines*ncpu:]
+   obuf[0] = obuf[0][:(lineSize+1)*bufLines*ncpu]
+   activeobuf := 0
+
    line, err := in.ReadSlice('\n')
    for err == nil {
-      out.Write(line)
+      title := make([]byte, len(line))
+      copy(title, line)
+
+   BulkRead:
       for {
-         line, err = in.ReadSlice('\n')
-         if err != nil || line[0] == '>' {
-            break
+         for lines := cap(buf)/lineSize - len(buf)/lineSize; lines > 0; lines-- {
+            line, err = in.Peek(61)
+            if err != nil {
+               line, err = in.ReadSlice('\n')
+               if err != nil || line[0] == '>' {
+                  break BulkRead
+               }
+               buf = append(buf, line[0:len(line)-1]...)
+               continue
+            }
+            if line[0] == '>' {
+               line, err = in.ReadSlice('\n')
+               break BulkRead
+            }
+            if line[lineSize] == '\n' {
+               buf = append(buf, line[0:lineSize]...)
+               in.Discard(lineSize + 1)
+               continue
+            }
+            line, err = in.ReadSlice('\n')
+            buf = append(buf, line[0:len(line)-1]...)
          }
-         if len(buf)+60 > cap(buf) {
-            nbuf := make([]byte, len(buf), cap(buf)+64*1024*1024)
-            copy(nbuf, buf)
-            buf = nbuf
-         }
-         buf = append(buf, line[0:len(line)-1]...)
+         nbuf := make([]byte, len(buf), cap(buf)+lineSize*1024*1024)
+         copy(nbuf, buf)
+         buf = nbuf
       }
 
-      lines := len(buf) / 60
-      charsleft := len(buf) % 60
-      obuf := make([]byte, len(buf)+lines+1)
+      <-outstatec
+      outc <- title
 
-      c := make(chan int, ncpu)
-      obufstart := 0
-      bufend := len(buf)
-      linesperthread := lines / ncpu
-      for i := 1; i <= ncpu; i++ {
-         if i == ncpu {
-            linesperthread += lines % ncpu
+      charsleft := len(buf) % lineSize
+
+      for bufend, lines := len(buf), len(buf)/lineSize; lines > 0; {
+         linesperthread := bufLines
+         linesptmod := 0
+         if lines < bufLines*ncpu {
+            linesperthread = lines / ncpu
+            linesptmod = lines % ncpu
          }
-         bufstart := bufend - linesperthread*60
-         obufend := obufstart + linesperthread*61
-         go createoutput(buf[bufstart:bufend], obuf[obufstart:obufend], c)
-         bufend = bufstart
-         obufstart = obufend
+         obufstart := 0
+         obufend := 0
+         for i := 0; i < ncpu; i++ {
+            if i == (ncpu - 1) {
+               linesperthread += linesptmod
+            }
+            lines -= linesperthread
+            bufstart := bufend - linesperthread*lineSize
+            obufend = obufstart + linesperthread*(lineSize+1)
+            prepworkorder := [][]byte{buf[bufstart:bufend], obuf[activeobuf][obufstart:obufend]}
+            cpuic[i] <- prepworkorder
+            bufend = bufstart
+            obufstart = obufend
+         }
+         for i := 0; i < ncpu; i++ {
+            <-prepstatec
+         }
+         <-outstatec
+         outc <- obuf[activeobuf][0:obufend]
+         activeobuf = activeobuf ^ 1
       }
-      for i := 0; i < ncpu; i++ {
-         <-c
-      }
-
       if charsleft > 0 {
-         obufpos := lines * 61
-         for bufpos := charsleft - 1; bufpos >= 0; bufpos-- {
-            obuf[obufpos] = complement[buf[bufpos]]
-            obufpos++
-         }
-         obuf[obufpos] = '\n'
-         out.Write(obuf)
-      } else {
-         out.Write(obuf[0 : len(obuf)-1])
+         prepworkorder := [][]byte{buf[0:charsleft], obuf[activeobuf][0 : charsleft+1]}
+         cpuic[0] <- prepworkorder
+         <-prepstatec
+         <-outstatec
+         outc <- obuf[activeobuf][0 : charsleft+1]
       }
       buf = buf[0:0]
    }
-   out.Flush()
+   <-outstatec
 }
