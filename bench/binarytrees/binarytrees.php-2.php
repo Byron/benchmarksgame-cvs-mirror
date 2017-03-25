5573ad4898b03daae67bf8e2ea0b10c7944f8398
igouy-guest <igouy-guest>
2017-03-25 04:13:32 +0000

    binary-trees reset
    SKIPPED:
    	website/websites/u64q/code/binarytrees.1.erlang.code
    	website/websites/u64q/code/binarytrees.1.erlang.log
    	website/websites/u64q/code/binarytrees.1.hipe.code
    	website/websites/u64q/code/binarytrees.1.hipe.log
    	website/websites/u64q/code/binarytrees.1.ifc.code
    	website/websites/u64q/code/binarytrees.1.ifc.log
    	website/websites/u64q/code/binarytrees.1.yarv.log
    	website/websites/u64q/code/binarytrees.2.erlang.code
    	website/websites/u64q/code/binarytrees.2.erlang.log
    	website/websites/u64q/code/binarytrees.2.hipe.code
    	website/websites/u64q/code/binarytrees.2.hipe.log
    	website/websites/u64q/code/binarytrees.2.ocaml.code
    	website/websites/u64q/code/binarytrees.2.ocaml.log
    	website/websites/u64q/code/binarytrees.2.php.code
    	website/websites/u64q/code/binarytrees.2.php.log
    	website/websites/u64q/code/binarytrees.4.php.code
    	website/websites/u64q/code/binarytrees.4.php.log
    	website/websites/u64q/code/binarytrees.5.ocaml.code
    	website/websites/u64q/code/binarytrees.5.ocaml.log
    	website/websites/u64q/data/data.csv
    	website/websites/u64q/data/ndata.csv
    	website/websites/u64q/data/u64q_bulkdata.csv.bz2

--- bench/binarytrees/binarytrees.php-2.php
+++ bench/binarytrees/binarytrees.php-2.php
@@ -5,25 +5,24 @@
    contributed by Peter Baltruschat
    modified by Levi Cameron
    modified by Craig Russell
+   *reset*
 */
 
 class Tree {
-   public $i;
    public $l;
    public $r;
    
-   public function __construct($item, $depth) {
-      $this->i = $item;
+   public function __construct($depth) {
       if($depth) {
-         $this->l = new Tree($item * 2 - 1, --$depth);
-         $this->r = new Tree($item * 2, $depth);
+         $this->l = new Tree(--$depth);
+         $this->r = new Tree($depth);
       }
    }
    
    public function check() {
-      return $this->i
-         + ($this->l->l === null ? $this->l->check() : $this->l->i)
-         - ($this->r->l === null ? $this->r->check() : $this->r->i);
+      return 1
+         + ($this->l->l === null ? $this->l->check() : 1)
+         + ($this->r->l === null ? $this->r->check() : 1);
    }
 }
 
@@ -33,12 +32,12 @@ $n = $argc == 2 ? $argv[1] : 1;
 $maxDepth = $minDepth + 2 > $n ? $minDepth + 2 : $n;
 $stretchDepth = $maxDepth + 1;
 
-$stretch = new Tree(0, $stretchDepth);
+$stretch = new Tree($stretchDepth);
 printf("stretch tree of depth %d\t check: %d\n",
    $stretchDepth, $stretch->check());
 unset($stretch);
 
-$longLivedTree = new Tree(0, $maxDepth);
+$longLivedTree = new Tree($maxDepth);
 
 $iterations = 1 << $maxDepth;
 do
@@ -46,12 +45,11 @@ do
    $check = 0;
    for($i = 1; $i <= $iterations; ++$i)
    {
-      $check += (new Tree($i, $minDepth))->check()
-         + (new Tree(-$i, $minDepth))->check();
+      $check += (new Tree($minDepth))->check();
    }
    
    printf("%d\t trees of depth %d\t check: %d\n",
-      $iterations<<1, $minDepth, $check);
+      $iterations, $minDepth, $check);
    
    $minDepth += 2;
    $iterations >>= 2;
