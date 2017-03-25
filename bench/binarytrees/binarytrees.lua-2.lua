cc601b56e2bb2b215896aff8ada6d972b0eb3475
igouy-guest <igouy-guest>
2017-03-25 05:26:30 +0000

    binary-trees reset
    SKIPPED:
    	website/websites/u64q/binarytrees.html
    	website/websites/u64q/code/binarytrees.1.ghc.code
    	website/websites/u64q/code/binarytrees.1.ghc.log
    	website/websites/u64q/code/binarytrees.1.ifc.code
    	website/websites/u64q/code/binarytrees.2.fsharpcore.code
    	website/websites/u64q/code/binarytrees.2.fsharpcore.log
    	website/websites/u64q/code/binarytrees.2.lua.code
    	website/websites/u64q/code/binarytrees.2.lua.log
    	website/websites/u64q/code/binarytrees.3.fsharpcore.log
    	website/websites/u64q/data/data.csv
    	website/websites/u64q/data/ndata.csv
    	website/websites/u64q/data/u64q_bulkdata.csv.bz2

--- bench/binarytrees/binarytrees.lua-2.lua
+++ bench/binarytrees/binarytrees.lua-2.lua
@@ -1,23 +1,23 @@
 -- The Computer Language Benchmarks Game
 -- http://benchmarksgame.alioth.debian.org/
 -- contributed by Mike Pall
+-- *reset*
 
-local function BottomUpTree(item, depth)
+local function BottomUpTree(depth)
   if depth > 0 then
-    local i = item + item
     depth = depth - 1
-    local left, right = BottomUpTree(i-1, depth), BottomUpTree(i, depth)
-    return { item, left, right }
+    local left, right = BottomUpTree(depth), BottomUpTree(depth)
+    return { left, right }
   else
-    return { item }
+    return { }
   end
 end
 
 local function ItemCheck(tree)
-  if tree[2] then
-    return tree[1] + ItemCheck(tree[2]) - ItemCheck(tree[3])
+  if tree[1] then
+    return 1 + ItemCheck(tree[1]) + ItemCheck(tree[2])
   else
-    return tree[1]
+    return 1
   end
 end
 
@@ -28,22 +28,21 @@ if maxdepth < N then maxdepth = N end
 
 do
   local stretchdepth = maxdepth + 1
-  local stretchtree = BottomUpTree(0, stretchdepth)
+  local stretchtree = BottomUpTree(stretchdepth)
   io.write(string.format("stretch tree of depth %d\t check: %d\n",
     stretchdepth, ItemCheck(stretchtree)))
 end
 
-local longlivedtree = BottomUpTree(0, maxdepth)
+local longlivedtree = BottomUpTree(maxdepth)
 
 for depth=mindepth,maxdepth,2 do
   local iterations = 2 ^ (maxdepth - depth + mindepth)
   local check = 0
   for i=1,iterations do
-    check = check + ItemCheck(BottomUpTree(1, depth)) +
-            ItemCheck(BottomUpTree(-1, depth))
+    check = check + ItemCheck(BottomUpTree(depth))
   end
   io.write(string.format("%d\t trees of depth %d\t check: %d\n",
-    iterations*2, depth, check))
+    iterations, depth, check))
 end
 
 io.write(string.format("long lived tree of depth %d\t check: %d\n",
