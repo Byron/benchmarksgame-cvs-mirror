3f14b2dfd82ed9ad52924ffffa37dba0d3b5aa8e
Sebastian Thiel <byronimo@gmail.com>
2017-04-09 19:33:26 +0200

    Try to get cvsimport back to work

--- update-git-from-cvs-in-docker.sh
+++ update-git-from-cvs-in-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-git checkout origin
+git checkout origin || { git checkout --orphan origin && git commit --allow-empty -m "initial commit"; }
 git cvsimport -R -a -v -S '^website/' -d ":pserver:anonymous@cvs.debian.org:/cvs/benchmarksgame"  benchmarksgame
