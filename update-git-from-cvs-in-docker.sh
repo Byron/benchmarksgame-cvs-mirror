362bf9c9f48bb2c7aafa64ff7c82184aadb01f81
Sebastian Thiel <byronimo@gmail.com>
2017-04-23 18:54:55 +0200

    Also create the origin branch that it so much desires

--- update-git-from-cvs-in-docker.sh
+++ update-git-from-cvs-in-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-[ -n "$TRAVIS" ] && git fetch origin origin:refs/remotes/origin/origin && git checkout master
+[ -n "$TRAVIS" ] && git fetch origin origin:refs/remotes/origin/origin && git checkout -b origin origin/origin && git checkout master
 git cvsimport -R -a -v -S '^website/' -d ":pserver:anonymous@cvs.debian.org:/cvs/benchmarksgame"  benchmarksgame
